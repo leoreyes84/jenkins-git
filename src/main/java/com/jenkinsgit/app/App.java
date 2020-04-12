1420933f7b83b40891abddfdb7dce7689e67b0b9
Leonardo Reyes <Leonardo.ReyesRivero@wnco.com>
2020-04-12 16:09:07 -0500

    test prueba ejecucion

--- src/main/java/com/jenkinsgit/app/App.java
+++ src/main/java/com/jenkinsgit/app/App.java
@@ -2,8 +2,16 @@ package com.jenkinsgit.app;
 
 public class App {
 
-	public static void main(String[] args) {
-		System.out.println(" Hello world ");
+//	public static void main(String[] args) {
+//		System.out.println(" Hello world ");
+//	}
+
+	public int multiply(int x, int y) {
+		// the following is just an example
+		if (x > 999) {
+			throw new IllegalArgumentException("X should be less than 1000");
+		}
+		return x * y;
 	}
 
 }
--- src/test/java/com/jenkinsgit/app/AppTest.java
+++ src/test/java/com/jenkinsgit/app/AppTest.java
@@ -1,5 +1,6 @@
 package com.jenkinsgit.app;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,14 +8,24 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+public class AppTest {
+	/**
+	 * Rigorous Test :-)
+	 */
+//    @Test
+//    public void shouldAnswerWithTrue()
+//    {
+//        assertTrue( true );
+//    }
+	@Test(expected = IllegalArgumentException.class)
+	public void testExceptionIsThrown() {
+		App tester = new App();
+		tester.multiply(1000, 5);
+	}
+
+	@Test
+	public void testMultiply() {
+		App tester = new App();
+		assertEquals("10 x 5 must be 50", 50, tester.multiply(10, 5));
+	}
 }
